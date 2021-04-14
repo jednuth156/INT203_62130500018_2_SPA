ef492e4c3ac497b0238aae286c4755793ab6ecdd
jednuth156 <jednuth.tatirat@mail.kmutt.ac.th>
2021-04-15 00:49:01 +0700

    [ADD]Form Create User

--- src/components/EditUser.vue
+++ src/components/EditUser.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="row justify-content-center">
+    <div class="justify-center row">
         <div class="col-md-6">
             <!-- Update here -->
+            <h1>Edit User</h1>
         </div>
     </div>
 </template>
--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <!-- Display all list -->
+            <h1>Show List</h1>
         </div>
     </div>
 </template>
