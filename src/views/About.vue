050661a8af5b66893d0ad4abf0e16631e15a2c4e
jednuth156 <jednuth.tatirat@mail.kmutt.ac.th>
2021-04-17 23:19:22 +0700

    [ADD] CRUD POST

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,60 @@
 <template>
-<nav-bar :navber="navbar" />
+    <nav-bar :navber="navbar"/>
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <!-- Display all list -->
-            <h1>Show List</h1>
-            
+            <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Show List</h1>
+   <div class="row">
+        <div class="col-md-12">
+          <table class="table-fixed">
+            <thead>
+              <tr class="text-white bg-black">
+                <th class="w-1/2 ...">FullName</th>
+                <th class="w-1/3 ...">Email</th>
+                <th class="w-1/4 ...">Phone</th>
+                <th class="w-1/4 ...">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="results in result" :key="results.id" class="bg-blue-200">
+                <th>
+                  {{results.Fullname}}
+                </th>
+                <th>{{results.Email}}</th>
+                <th>{{results.Phone}}</th>
+              </tr>
+            </tbody>
+          </table>
+          
         </div>
+      </div>
     </div>
+  </div>
+    
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            
+    name: "about",
+    data(){
+        return{
+            url : "http://localhost:5000/user",
+            result : []
         }
     },
+    methods : {
+        async getInfoResult() {
+            try {
+                const res = await fetch(this.url);
+                const data = await res.json();
+                return data;
+            } catch (error) {
+                console.log(`error ${error}`)
+            }
+        }
+    },
+    async created() {
+        this.result = await this.getInfoResult();
+    }
+    
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,69 +1,106 @@
 <template>
-<nav-bar :navber="navbar" />
-    <div class="flex items-center min-h-full bg-gray-50 dark:bg-gray-900">
-        <div class="container mx-auto">
-            <div class="max-w-md p-5 mx-auto my-10 bg-white rounded-md shadow-sm">
+  <nav-bar :navber="navbar" />
+  <div class="flex items-center min-h-full bg-gray-50 dark:bg-gray-900">
+    <div class="container mx-auto">
+      <div class="max-w-md p-5 mx-auto my-10 bg-white rounded-md shadow-sm">
         <div class="text-center">
-            <!-- Content goes here -->
-            <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Create Your User</h1>
-            <div class="m-7">
-                <form @submit.prevent="SubmitUser()">
-                    <div class="mb-6">
-                        <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Full Name</label>
-                        <input v-model="user.fullname" type="text" name="name" id="name" placeholder="Your Name" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
-                    </div>
-                    <div class="mb-6">
-                        <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
-                        <input v-model="user.email" type="email" name="email" id="email" placeholder="you@company.com" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
-                    </div>
-                    <div class="mb-6">
-
-                        <label for="phone" class="text-sm text-gray-600 dark:text-gray-400">Phone Number</label>
-                        <input v-model="user.phone" type="text" name="phone" id="phone" placeholder="+99...." required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
-                    </div>
-                    <div class="mb-6">
-                        <page-button ton="Create User"></page-button>
-                    </div>
-                    <p class="text-base text-center text-gray-400" id="result">
-                    </p>
-                </form>
-            </div>
+          <!-- Content goes here -->
+          <h1
+            class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200"
+          >
+            Create Your User
+          </h1>
+          <div class="m-7">
+            <form @submit="SubmitUser()">
+              <div class="mb-6">
+                <label
+                  for="name"
+                  class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >Full Name</label
+                >
+                <input
+                  v-model="user.fullname"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  for="email"
+                  class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >Email Address</label
+                >
+                <input
+                  v-model="user.email"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="you@company.com"
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  for="phone"
+                  class="text-sm text-gray-600 dark:text-gray-400"
+                  >Phone Number</label
+                >
+                <input
+                  v-model="user.phone"
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  placeholder="+99...."
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <page-button ton="Create User"></page-button>
+              </div>
+              <p class="text-base text-center text-gray-400" id="result"></p>
+            </form>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-    </div> 
+  </div>
 </template>
 
 <script>
-import PageButton from '../components/PageButton.vue'
+import PageButton from "../components/PageButton.vue";
 
-export default { name:'Home',
-    components:{PageButton},
-    data() {
-        return {
-            user : {
-                fullname : '',
-                email : '',
-                phone : '' ,
-                
-            },
-            url : 'http://localhost:5000/user',
-            result : [],
-            
-        }
+export default {
+  name: "Home",
+  components: { PageButton },
+  data() {
+    return {
+      user: {
+        fullname: "",
+        email: "",
+        phone: "",
+      },
+      url: "http://localhost:5000/user",
+      result: [],
+    };
+  },
+
+  methods: {
+    SubmitUser() {
+      this.addNewUser({
+        fullname: this.user.fullname,
+        email: this.user.email,
+        phone: this.user.phone,
+      });
+      this.$router.push('/about')
     },
 
-    methods:  {
-         SubmitUser() {
-            this.addNewUser({
-            fullname: this.user.fullname,
-            email: this.user.email,
-            phone: this.user.phone,
-        });
-        console.log(this.user);
-        },
-        
-        async addNewUser(newUser) {
+    async addNewUser(newUser) {
       try {
         const res = await fetch(this.url, {
           method: "POST",
@@ -81,9 +118,7 @@ export default { name:'Home',
       } catch (error) {
         console.log(`Colud not save! ${error}`);
       }
-    }
-
-    }
-
-}
+    },
+  },
+};
 </script>
