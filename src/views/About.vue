98c1671069d2f4e8622388a1f0527544b6e9f895
jednuth156 <jednuth.tatirat@mail.kmutt.ac.th>
2021-04-18 15:54:58 +0700

    [ADD]Edit User

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,60 +1,88 @@
 <template>
-    <nav-bar :navber="navbar"/>
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Show List</h1>
-   <div class="row">
+  <nav-bar :navber="navbar" />
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+        Show List
+      </h1>
+      <div class="row">
         <div class="col-md-12">
           <table class="table-fixed">
             <thead>
               <tr class="text-white bg-black">
-                <th class="w-1/2 ...">FullName</th>
-                <th class="w-1/3 ...">Email</th>
+                <th class="w-1/4 ...">FullName</th>
+                <th class="w-1/4 ...">Email</th>
                 <th class="w-1/4 ...">Phone</th>
                 <th class="w-1/4 ...">Action</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="results in result" :key="results.id" class="bg-blue-200">
+              <tr
+                v-for="results in result"
+                :key="results.id"
+                class="bg-blue-200"
+              >
                 <th>
-                  {{results.Fullname}}
+                  {{ results.Fullname }}
                 </th>
-                <th>{{results.Email}}</th>
-                <th>{{results.Phone}}</th>
+                <th>{{ results.Email }}</th>
+                <th>{{ results.Phone }}</th>
+                <th>
+                  <button
+                    @click="deletedUser(results.id)"
+                    class="m-1 bg-white"
+                  >
+                    <span class="material-icons-outlined">  <img src="@/assets/delete.svg" alt="" /> </span>
+                    
+                  </button>
+                </th>
               </tr>
             </tbody>
           </table>
-          
         </div>
       </div>
     </div>
   </div>
-    
 </template>
 
 <script>
 export default {
-    name: "about",
-    data(){
-        return{
-            url : "http://localhost:5000/user",
-            result : []
-        }
+  name: "about",
+  data() {
+    return {
+      url: "http://localhost:5000/user",
+      result: [],
+    };
+  },
+  methods: {
+    async getInfoUser() {
+      try {
+        const res = await fetch(this.url);
+        const data = await res.json();
+        return data;
+      } catch (error) {
+        console.log(`error ${error}`);
+      }
     },
-    methods : {
-        async getInfoUser() {
-            try {
-                const res = await fetch(this.url);
-                const data = await res.json();
-                return data;
-            } catch (error) {
-                console.log(`error ${error}`)
-            }
-        }
+    async deletedUser(deleteuser) {
+      try {
+        await fetch(`${this.url}/${deleteuser}`, {
+          method: "DELETE",
+        });
+        this.result = this.result.filter(
+          (results) => results.id !== deleteuser
+        );
+      } catch (error) {
+        console.log(`Could not delete! ${error}`);
+      }
     },
-    async created() {
-        this.result = await this.getInfoUser();
-    }
-    
-}
+  },
+  async created() {
+    this.result = await this.getInfoUser();
+  },
+};
 </script>
+
+<style scoped>
+
+</style>
